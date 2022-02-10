c4cc522b1e4b5f0e41168f62f98b7fdb1da7de22
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2022-02-10 12:12:30 +0900

    class

---
+++ src/classalgorithm/classhuman.java
@@ -0,0 +1,32 @@
+package classalgorithm;
+// 사람클래스
+class human {
+	String name; // 이름
+	int height; // 신장
+	int weight; // 체중
+}
+
+class humantester {
+	public static void main(String[] args) {
+		
+	human GD = new human();
+	human YDG = new human();
+	
+	GD.name = "지용";
+	GD.height = 170;
+	GD.weight = 60;
+	
+	YDG.name = "두기";
+	YDG.height = 188;
+	YDG.weight = 68;
+	
+	System.out.println("이름:"+GD.name); // GD의 데이터 
+	System.out.println("키:"+GD.height);
+	System.out.println("체중:"+GD.weight);
+	System.out.println();
+
+	System.out.println("이름:"+YDG.name); // YDG의 데이터
+	System.out.println("키:"+YDG.height);
+	System.out.println("체중:"+YDG.weight);
+	}
+}
---
+++ src/hello/refresh.java
@@ -0,0 +1,12 @@
+package hello;
+//2개의 변수 x와 y의 합과 평균을 구하자. x와 y에서는 적당한 값을 대입해서 사용한다.
+public class refresh {
+	public static void main(String[] args) {
+		int x = 5;
+		int y = 7;
+				
+		System.out.println(x+y);
+		System.out.println((x+y)/2);
+		
+	}
+}
