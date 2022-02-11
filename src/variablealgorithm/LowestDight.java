4630c232388536125adf7d574b4e66ee5a44f4bd
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2022-02-11 17:26:13 +0900

    new Scanner
    연산자 응용

--- src/variablealgorithm/LowestDight.java
+++ src/variablealgorithm/LowestDight.java
@@ -7,12 +7,12 @@ public class LowestDight {
 
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
-		System.out.print("머리속에 떠오르는 정수를 입력 해보아라");
+		System.out.print("머리속에 떠오르는 정수를 입력 해보아라 ");
 		int a = stdIn.nextInt();
-		System.out.println("입력한 정수는" + a + "입니다.");
+		System.out.println("입력한 정수는 " + a + " 입니다.");
 		
-		System.out.println("입력한 정수의 마지막 자릿수를 제외 한 값" + a + "입니다.");
-		System.out.println("입력한 정수의 마지막 자릿수" + a + "입니다.");
+		System.out.println("입력한 정수의 마지막 자릿수를 제외 한 값은 " + (a / 10) + " 입니다.");
+		System.out.println("입력한 정수의 마지막 자릿수는 " +  (a % 10) + " 입니다.");
 	}
 
 }
